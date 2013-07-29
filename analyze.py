85288dd778a2b5712842215f0803a24ec15b5bad
James Montalvo <edwin.j.montalvo@nasa.gov>
2013-07-29 17:03:10 -0500

    each file now has a sha1 hash in database

--- analyze.py
+++ analyze.py
@@ -11,9 +11,13 @@ cur = conn.cursor()
 # top level directory to search (have to escape backslashes, f-u Windows)
 # WAS: root = 'C:\\jamesbatch\\sdrive\\ConsoleLogBackup'
 root = config.directoryToAnalyze
+print "Performing analysis on ", root
 
 for fullpath, dirs, files in os.walk(root):
-    for name in files:
+	for name in files:
+		
+		filepath = join(fullpath, name)
+	
 		# get the file extension
 		# use os.path.splitext() to split the filename on the last period
 		# returns an array of two items; take the second by doing [1]
@@ -21,28 +25,31 @@ for fullpath, dirs, files in os.walk(root):
 		# make it all lowercase
 		ext = os.path.splitext(name)[1][1:].lower()
 		
-		size = getsize(join(fullpath, name)) # size of this file
+		size = getsize( filepath ) # size of this file
 		
 		# path to files with top level removed
 		# this will make it easier to translate between S-drive files being
 		# analyzed on a computer other than JSC-MOD-FS3
 		relativepath = fullpath[len(root):]
 		
-		stats = os.stat(join(fullpath, name))
+		stats = os.stat( filepath )
 		
 		sha1 = hashlib.sha1()
-		sha1.update("")
+		sha1.update( file( filepath , 'rb').read() )
+		sha1 = sha1.hexdigest()		
 		
 		created = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(stats.st_ctime))
 		modified = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(stats.st_mtime))
 		accessed = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime(stats.st_atime))
 		
-		cur.execute("""
+		query = """
 			INSERT INTO files 
 			(filename,extension,bytes,root,relativepath,sha1,created,modified,accessed) 
 			VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-			""", 
-			(name,ext,size,root,relativepath,sha1.hexdigest(),created,modified,accessed))
+			"""
+		
+		cur.execute(query, 
+			(name,ext,size,root,relativepath,sha1,created,modified,accessed))
 
 # Make the changes to the database persistent
 conn.commit()
